3fcdae3765cf117c6313c28caff27a8ea7dd650c
zizotopia <zizoknightboy07@yahoo.com>
2024-04-20 19:35:11 -0700

    3

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
-eEres the server for the project
+-- Prepares the server for the project
 -- Creates a database hbnb_test_db and a new user hbnb_test
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
