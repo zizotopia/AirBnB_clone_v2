bd3d74233bd0ad7e23eab1cb15b7add916a7cc65
zizotopia <zizoknightboy07@yahoo.com>
2024-05-22 09:58:19 -0700

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,53 +1,35 @@
 #!/usr/bin/python3
-"""
-    Creates and distributes an archive to the web servers
-"""
-
-from fabric.api import env, local, put, run
-from datetime import datetime
-from os.path import exists, isdir
-env.hosts = ['3.229.113.167', '3.234.210.158']
-env.user = 'ubuntu'
-env.identity = '~/.ssh/school'
+""" Starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
 
 
-def do_pack():
-    """generates a tgz archive"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except FileNotFoundError:
-        return None
+@app.route('/', strict_slashes=False)
+def hello_hbn():
+    """ Returns Hello HBNB! from 0.0.0.0:5000 """
+    return "Hello HBNB!"
 
 
-def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except FileNotFoundError:
-        return False
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns HBNB from 0.0.0.0:5000/hbnb """
+    return "HBNB"
 
 
-def deploy():
-    """creates and distributes an archive to the web servers"""
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-    return do_deploy(archive_path)
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """ Returns C followed by the value of text """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text="is cool"):
+    """ Returns Python followed by the vale of the text """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
